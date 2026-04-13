3891199b81605429fc2511539474e32189495c22
spacenpr <spacenpr@gmail.com>
2026-04-13 18:03:14 +0300

    Fix: bind to 0.0.0.0 and use PORT env var

--- model_api.py
+++ model_api.py
@@ -145,6 +145,5 @@ def predict(client: ClientData):
 # ----------------------------------------------------------------
 if __name__ == "__main__":
     import uvicorn
-
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
